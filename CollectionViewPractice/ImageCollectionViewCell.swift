34bd0854dd651a3611edd646e91980e3a60580f6
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2018-12-27 18:59:35 +0900

    ImageView追加

---
+++ CollectionViewPractice/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  CollectionViewPractice
+//
+//  Created by Akiko Hiura on 2018/12/27.
+//  Copyright © 2018年 藤田和磨. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
